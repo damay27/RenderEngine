e49a47312b3e05abb037b76b6548d759b7706717
damay27 <damay27@outlook.com>
2018-02-01 12:15:03 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,6 @@
 #include "Triangle.hpp"
 #include "Vector3D.h"
 #include "Raycast.h"
-//#include "SFML\Graphics.hpp"
-//#include "RaycastCamera.h"
-#include "matrix.h"
-#include "matrixSizeException.h"
-#include "TransformMatrix.h"
 #include "Camera.h"
 #include "Transform3D.h"
 #include "Light.h"
@@ -16,106 +11,6 @@ using namespace std;
 
 int main()
 {
-	/*Vector3D a(5, -1, 0);
-	Vector3D b(1, 1, 0);
-	Vector3D c(1, 0, 1);
-	Vector3D x(c);
-	Triangle t(a, b, c, x);
-
-	Vector3D origin(0, 0, 0);
-	float planeWidth = 1;
-	float planeHieght = .75;
-	float focalLenght = .25;
-	int resH = 640;
-	int resV = 480;
-	float stepH = planeWidth / resH;
-	float stepV = planeHieght / resV;
-	float posX = -planeWidth/2;
-	float posY = planeHieght / 2;
-	sf::Image img;
-	img.create(resH, resV);
-
-	cout << "Vertices:" << endl;
-	cout << "<" << a.getX() << ", " << a.getY() << ", " << a.getZ() << ">" << endl;
-	cout << "<" << b.getX() << ", " << b.getY() << ", " << b.getZ() << ">" << endl;
-	cout << "<" << c.getX() << ", " << c.getY() << ", " << c.getZ() << ">" << endl;
-
-	for (int x = 0; x < resH; x++) {
-
-		for (int y = 0; y < resV; y++) {
-			Vector3D cast = Raycast(origin, Vector3D(focalLenght, posX, posY), t);
-
-			if (cast.isNull()==false) {
-				img.setPixel(x, y, sf::Color::White);
-			}
-
-			posY -= stepV;
-
-		}
-
-		posX += stepH;
-		posY = planeHieght / 2;
-	}
-
-
-	sf::RenderWindow window(sf::VideoMode(resH, resV), "My window");
-	sf::Texture tex;
-	tex.create(resH, resV);
-	tex.update(img);
-
-	sf::Event event;
-	while (1) {
-		window.pollEvent(event);
-		if (event.type == sf::Event::Closed) {
-			window.close();
-		}
-
-		window.clear();
-		window.draw(sf::Sprite(tex));
-		window.display();
-
-	}*/
-
-	/*RaycastCamera camera(640, 480, 90, 67.5);
-	camera.setAngles(90, 0);
-	camera.setPosition(0, 1, 0);
-
-	Vector3D a(5, 1, 0);
-	Vector3D b(5, -1, 0);
-	Vector3D c(5, 0, 1);
-	Triangle t(a, b, c);
-
-	Vector3D vec = Raycast(Vector3D(0, 0, 0), Vector3D(1, 0, 0), t);
-
-	Object object(vector<Triangle>({ t }), Vector3D(1, 0, 0));
-
-	RenderImage rImg;
-
-	rImg=camera.Render(vector<Object>({ object }));
-
-	sf::Image img = rImg.getImage();
-
-	sf::RenderWindow window(sf::VideoMode(640,480), "My window");
-	sf::Texture tex;
-	tex.create(640, 480);
-	tex.update(img);
-
-	sf::Event event;
-	while (1) {
-		window.pollEvent(event);
-		if (event.type == sf::Event::Closed) {
-			window.close();
-		}
-
-		window.clear();
-		window.draw(sf::Sprite(tex));
-		window.display();
-
-	}
-
-
-	return 0;*/
-
 	//create and position the camera
 	Transform3D cameraTransform;
 	cameraTransform.translate(-5, 0, 0);
